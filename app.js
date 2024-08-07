256ed7a5a99c1b36d60720a526f70a795809db17
Moiz Sajid  Salam <91916311+Moizsajid1234@users.noreply.github.com>
2024-08-07 18:38:49 +0500

    Random Dice

---
+++ app.js
@@ -0,0 +1,14 @@
+let randomDice = Math.ceil(Math.random() * 6);
+if(randomDice === 1){
+    document.write('<img src="assets/1-removebg-preview.png" alt="1"/>')
+}else if(randomDice === 2){
+    document.write('<img src="assets/2-removebg-preview.png" alt="1"/>')
+}else if(randomDice === 3){
+    document.write('<img src="assets/3-removebg-preview.png" alt="1"/>')
+}else if(randomDice === 4){
+    document.write('<img src="assets/4-removebg-preview.png" alt="1"/>')
+}else if(randomDice === 5){
+    document.write('<img src="assets/5-removebg-preview.png" alt="1"/>')
+}else if(randomDice === 6){
+    document.write('<img src="assets/6-removebg-preview.png" alt="1"/>')
+}
